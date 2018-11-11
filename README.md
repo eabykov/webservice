5dc0db90917fe72481c1b5ad3f1ca01faf0ff0e4
Eugene Bykov <ea.bykov@gmail.com>
2018-11-11 12:05:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# WebService
+## Test Web Page with Basic Authentication written on Python using Flask
+### Show the Flask app PID, CPU and MEM usage
+
+username: **admin**
+
+password: **secret**
+
+**Note:** To enable Authentication again after success login clear the browser cache.
+
+## Commands for deployment Flask application in Debian based Linux (Ubuntu)
+
+    sudo apt update
+    sudo apt -y install python3 python3-venv python3-dev gcc
+
+## Commands for deployment Flask application in RedHat based Linux (CentOS)
+
+    sudo yum update
+    sudo yum -y install python3 python3-venv python3-devel gcc
+
+## Commands for deployment Flask application
+
+    cd ~/webservice
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -U pip
+    pip install -r requirements.txt
+    export FLASK_APP=webservice.py
+    export FLASK_ENV=development
+    echo "export FLASK_APP=webservice.py" >> ~/.bash_profile
+    echo "export FLASK_AENV=development" >> ~/.bash_profile
+
+## Commands for run Flask application on port 80 
+    cd ~/webservice
+    sudo /bin/python3 ~/webservice/venv/bin/flask run --port=80
---
+++ app/routes.py
@@ -0,0 +1,38 @@
+from app import app
+import psutil
+from subprocess import Popen, PIPE
+from functools import wraps
+from flask import Flask, request, Response, render_template
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+ospid = psutil.Process()
+cpuTimes = ospid.cpu_times()
+memoryUsage = ospid.memory_full_info()
+lastCommit = Popen(['git','log','--pretty=oneline','-n','1'],stdout=PIPE).communicate()
+
+@app.route('/')
+@app.route('/index')
+@requires_auth  #requires_auth decorator for basic auth
+def index():
+    return render_template('index.html', lastCommit=lastCommit,ospid=ospid, cpuTimes=cpuTimes, memoryUsage=memoryUsage)
